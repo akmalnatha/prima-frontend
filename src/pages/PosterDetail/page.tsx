7e7b885db81280990f6a2908b61f2006d4c72ebd
sulsul2 <18221110@std.stei.itb.ac.id>
2023-12-17 14:53:15 +0700

    feat bg fix all

--- src/pages/PosterDetail/page.tsx
+++ src/pages/PosterDetail/page.tsx
@@ -8,7 +8,34 @@ export default function PosterDetail() {
   return (
     <>
       <Navbar idx={5} />
-      <div className="w-full h-full flex flex-col px-10 lg:px-24 mb-[100px] lg:mb-[180px] xl:mb-[900px]">
+      <div className="w-[300px] h-[300px] md:w-[500px] md:h-[500px] absolute top-[0px] right-[0px] z-0 bg-[url('/assets/spiraldetail.svg')] bg-cover" />
+      <div className="w-[300px] h-[600px] md:w-[600px] md:h-[1000px] lg:w-[1000px] lg:h-[1200px] absolute top-[550px] md:top-[800px] lg:top-[1000px] xl:top-[1400px] right-[0px] -z-10 bg-[url('/assets/spiraldetail3.svg')] bg-cover" />
+      <div className="w-full h-full flex flex-col px-10 lg:px-24 mb-[100px] lg:mb-[180px]">
+      <img
+          src="/assets/spiraldetail2.svg"
+          alt=""
+          className="absolute top-[200px] md:top-[80px] xl:top-[-100px] left-[0px] z-0"
+        />
+        <img
+          src="/assets/radial.svg"
+          alt=""
+          className="absolute top-[500px] lg:top-[1000px] left-[0px] z-0"
+        />
+        <img
+          src="/assets/radial1.svg"
+          alt=""
+          className="absolute top-[600px] xl:top-[1500px] right-[0px] z-0 hidden md:block"
+        />
+        <img
+          src="/assets/spiraldetail4.svg"
+          alt=""
+          className="w-[700px] h-[700px] absolute top-[1000px] lg:top-[1400px] xl:top-[2000px] left-[-200px] -z-10 hidden md:block"
+        />
+        <img
+          src="/assets/radial.svg"
+          alt=""
+          className="absolute top-[1000px] lg:top-[1500px] xl:top-[2100px] left-[0px] z-0 hidden md:block"
+        />
         <p className="mt-[96px] lg:mt-[160px] font-semibold text-[16px] lg:text-[36px] text-center">
           Pengembangan Teknologi Material Bioaspal - Sebagai Bahan Peremaja
           untuk Material Daur Ulang Perkerasan Jalan Lentur - Part 2
@@ -16,16 +43,18 @@ export default function PosterDetail() {
         <p className="mt-4 font-medium text-[12px] lg:text-[24px] text-center">
           Oleh: Bambang Sugeng, Eri Susanto, Atmy Verani R.S., dan Ivan Imanuel
         </p>
-        <div className="mt-12 w-full flex justify-between">
-          <Button
-            type={"button"}
-            icon={true}
-            iconPosition="left"
-            text="Back to Exhibitor Posters"
-            color="secondary"
-            size="medium"
-            onClick={() => nav("/exhibitors")}
+        <div className="mt-12 w-full flex justify-center md:justify-between z-40">
+          <div className="hidden md:block">
+            <Button
+              type={"button"}
+              icon={true}
+              iconPosition="left"
+              text="Back to Exhibitor Posters"
+              color="secondary"
+              size="medium"
+              onClick={() => nav("/exhibitors-posters")}
             />
+          </div>
           <Button
             type={"button"}
             icon={true}
@@ -35,10 +64,14 @@ export default function PosterDetail() {
             size="medium"
           />
         </div>
-        <div className="mt-12 w-full h-fit pl-14">
-            <div className="w-fit h-fit border-[10px] border-[#3F4283]">
-                <img src="/assets/poster1.svg" alt="" className=" -translate-x-4 translate-y-4" />
-            </div>
+        <div className="mt-12 w-full h-fit z-40">
+          <div className="w-fit h-fit border-[10px] border-[#3F4283] mx-auto">
+            <img
+              src="/assets/poster1.svg"
+              alt=""
+              className=" -translate-x-4 translate-y-4"
+            />
+          </div>
         </div>
       </div>
       <Footer />
