70d6db0d80d8bbd6e57e2bcc4291dab083dd6c6f
akmalnatha <akmalabhirama2002@gmail.com>
2023-12-18 01:36:09 +0700

    style: card and button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,5 @@
 import { FormEventHandler, MouseEventHandler } from "react";
+import { BiLike } from "react-icons/bi";
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 
 
@@ -9,6 +10,7 @@ const Button = ({
   onSubmit,
   isLoading = false,
   color = "primary",
+  like = 0,
   icon,
   iconPosition = "right",
   size = "large",
@@ -20,7 +22,8 @@ const Button = ({
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
   onSubmit?: FormEventHandler<HTMLButtonElement> | undefined;
   isLoading?: boolean;
-  color?: "primary" | "secondary";
+  color?: "primary" | "secondary" | "like";
+  like?: number;
   icon?: boolean;
   iconPosition?: "right" | "left";
   size?: "large" | "medium" | "small";
@@ -28,72 +31,83 @@ const Button = ({
   fitContent?: boolean;
 }) => {
   return (
-    <button
-      disabled={disable || isLoading}
-      type={type}
-      onClick={onClick}
-      onSubmit={onSubmit}
-      className={`${size == "large" ? "px-8 py-3" : size == "medium" ? "px-5 py-2" : "px-3 py-2"} 
-      ${
-        disable
-          ? "bg-mono-grey"
-          : color == "primary"
-          ? "bg-gradient-to-b from-blue-primary to-blue-light active:from-blue-primary active:to-blue-primary hover:from-purple-primary hover:to-blue-light text-white"
-          : "bg-white border-2 border-black group hover:border-blue-primary active:border-indigo-700 text-black hover:text-blue-primary"
-      } rounded-full text-[12px] md:text-[16px] font-semibold ${fitContent ? "w-fit" : "w-full"} `}
-    >
-      {isLoading ? (
-        <div className={`flex items-center justify-center ${size == "large" ? "text-base md:text-2xl" : size == "medium" ? "text-xl" : "text-lg"}`}>
-          <svg
-            className="mr-3 h-5 w-5 animate-spin"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
+    <>
+      {color != "like" && <button
+        disabled={disable || isLoading}
+        type={type}
+        onClick={onClick}
+        onSubmit={onSubmit}
+        className={`${size == "large" ? "px-8 py-3" : size == "medium" ? "px-5 py-2" : "px-3 py-2"} 
+        ${
+          disable
+            ? "bg-mono-grey"
+            : color == "primary"
+            ? "bg-gradient-to-b from-blue-primary to-blue-light active:from-blue-primary active:to-blue-primary hover:from-purple-primary hover:to-blue-light text-white"
+            : "bg-white border-2 border-black group hover:border-blue-primary active:border-indigo-700 text-black hover:text-blue-primary"
+        } rounded-full text-[12px] md:text-[16px] font-semibold ${fitContent ? "w-fit" : "w-full"} `}
+      >
+        {isLoading ? (
+          <div className={`flex items-center justify-center ${size == "large" ? "text-base md:text-2xl" : size == "medium" ? "text-xl" : "text-sm lg:text-lg font-normal"}`}>
+            <svg
+              className="mr-3 h-5 w-5 animate-spin"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Loading...
+          </div>
+        ) : (
+          <div
+            className={`${
+              iconPosition == "right" ? "" : "flex-row-reverse"
+            } flex items-center justify-center gap-3 ${size == "large" ? "text-base md:text-2xl" : size == "medium" ? "text-xl" : "text-sm lg:text-lg font-normal"}`}
           >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-          Loading...
-        </div>
-      ) : (
-        <div
-          className={`${
-            iconPosition == "right" ? "" : "flex-row-reverse"
-          } flex items-center justify-center gap-3 ${size == "large" ? "text-base md:text-2xl" : size == "medium" ? "text-xl" : "text-lg font-normal"}`}
-        >
+            {text}
+            {icon && (
+              size == "large" || size == "medium" ?
+              <img
+                src={
+                  color == "primary"
+                    ? size == "large"
+                      ? "assets/button_arrow_white.svg"
+                      : "assets/button_arrow_circle.svg"
+                    : "assets/button_arrow_black.svg"
+                }
+                alt="arrow"
+                className={`${size == "large" && "py-2 px-4 border-2 border-white"} rounded-full`}
+              />
+              :
+              <div className={`${color == "primary" ? "border-2 border-white rounded-full px-1 py-1" : ""} shrink-0`}>
+                  <HiOutlineArrowLongRight/> 
+              </div>
+            )}
+          </div>
+        )}
+      </button>}
+      {color == "like" && 
+        <div className={`bg-white border-2 border-black group text-black rounded-full text-[12px] md:text-[16px] font-semibold ${fitContent ? "w-fit" : "w-full"} p-2 flex gap-2 items-center cursor-pointer`}>
           {text}
-          {icon && (
-            size == "large" || size == "medium" ?
-            <img
-              src={
-                color == "primary"
-                  ? size == "large"
-                    ? "assets/button_arrow_white.svg"
-                    : "assets/button_arrow_circle.svg"
-                  : "assets/button_arrow_black.svg"
-              }
-              alt="arrow"
-              className={`${size == "large" && "py-2 px-4 border-2 border-white"} rounded-full`}
-            />
-            :
-            <div className={`${color == "primary" ? "border-2 border-white rounded-full px-1 py-1" : ""} shrink-0`}>
-                <HiOutlineArrowLongRight/> 
-            </div>
-          )}
+          <button className="bg-gradient-to-b from-blue-primary to-blue-light active:from-blue-primary active:to-blue-primary hover:from-purple-primary hover:to-blue-light text-white flex flex-col gap-[2px] text-3xl w-[68px] h-[68px] rounded-full items-center justify-center" onClick={onClick} onSubmit={onSubmit}>
+              <BiLike/>
+              <p className="text-xs">{like} Likes</p>
+          </button>
         </div>
-      )}
-    </button>
+      }
+    </>
   );
 };
 
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,5 @@
-import { MouseEventHandler } from "react";
-import { BiLike } from "react-icons/bi";
+import { MouseEventHandler, useState } from "react";
+import { BiLike, BiSolidLike } from "react-icons/bi";
 import Button from "./button";
 import "./card.css"
 
@@ -10,6 +10,8 @@ const Card = ({
   likes = 0,
   link,
   onClick,
+  onClickLike,
+  liked
 }: {
   tipe: "exhibitors" | "most liked" | "posters";
   subTipe?: "departemen" | "content";
@@ -17,7 +19,20 @@ const Card = ({
   likes?: number;
   link?: string;
   onClick?: MouseEventHandler<HTMLDivElement> | undefined;
+  onClickLike?: MouseEventHandler<HTMLDivElement> | undefined;
+  liked?: boolean
 }) => {
+  const [likedPost, setLikedPost] = useState<boolean>(liked ? liked : false);
+  const [totalLikes, setTotalLikes] = useState<number>(likes);
+  function handleLikePost() {
+    onClickLike;
+    if(likedPost){
+      setTotalLikes(totalLikes-1);
+    } else {
+      setTotalLikes(totalLikes+1)
+    }
+    setLikedPost(!likedPost);
+  }
   return (
     <>
       {tipe == "most liked" && (
@@ -39,9 +54,9 @@ const Card = ({
                 {nama}
               </p>
             </div>
-            <div className="flex py-1 px-3 gap-2 text-black text-base lg:text-xl items-center border-2 border-black rounded-[14px]">
-              <BiLike/>
-              <p>{likes} Likes</p>
+            <div className={`flex py-1 px-3 gap-2 ${likedPost ? "text-purple-primary border-purple-primary" : "text-black border-black"} hover:text-blue-light active:text-blue-primary hover:border-blue-light active:border-blue-primary text-base lg:text-xl items-center border-2 rounded-[14px]`} onClick={() => handleLikePost()}>
+              {likedPost ? <BiSolidLike/> : <BiLike/>}
+              <p>{totalLikes} Likes</p>
             </div>
           </div>
         </div>
@@ -58,14 +73,14 @@ const Card = ({
                 alt="Card picture"
                 className="w-full h-[290px] object-cover rounded-t-[25px]"
               />
-              <p className="font-medium text-xl text-center w-full">
+              <p className="font-medium text-base lg:text-xl px-4 text-center w-full">
                 {nama}
               </p>
               <div className="flex justify-between items-center w-full px-6">
                 <Button type={"button"} color="primary" icon iconPosition="right" size="small" text="See Posters"/>
-                <div className="flex flex-col text-black text-3xl items-center ">
-                  <BiLike/>
-                  <p className="text-xs">{likes} Likes</p>
+                <div className={`flex flex-col ${likedPost ? "text-purple-primary" : "text-black"} hover:text-blue-light active:text-blue-primary text-3xl items-center`} onClick={() => handleLikePost()}>
+                  {likedPost ? <BiSolidLike/> : <BiLike/>}
+                  <p className="text-xs">{totalLikes} Likes</p>
                 </div>
               </div>
             </div>
@@ -83,7 +98,7 @@ const Card = ({
               alt="Card picture"
               className="w-full h-[670px] object-cover"
             />
-            <p className="font-medium text-xl text-center w-full">
+            <p className="font-medium text-base lg:text-xl px-4 text-center w-full">
               {nama}
             </p>
           </div>
